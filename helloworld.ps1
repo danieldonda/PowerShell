ddeda126e0ea09a9915c51f24a7055cfe59ae4bc
Daniel Donda <danieldonda@users.noreply.github.com>
2018-03-22 20:11:09 -0300

    Delete helloworld.ps1

--- helloworld.ps1
+++
@@ -1,7 +0,0 @@
-# Esse é um simples modelo de script com uma variável simples.
-
-#Declarações
-$variavel = "Hello World"
-
-#Inicio do script
-write-Output "Esse é o meu primeiro script e lá vai o famoso $variavel"
