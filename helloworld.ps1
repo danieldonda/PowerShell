c7194bde2bbe65a6c54d554afc6496f15e0e39e8
Daniel Donda <danieldonda@users.noreply.github.com>
2018-03-22 20:04:10 -0300

    Create helloworld.ps1

---
+++ helloworld.ps1
@@ -0,0 +1,7 @@
+# Esse é um simples modelo de script com uma variável simples.
+
+#Declarações
+$variavel = "Hello World"
+
+#Inicio do script
+write-Output "Esse é o meu primeiro script e lá vai o famoso $variavel"
